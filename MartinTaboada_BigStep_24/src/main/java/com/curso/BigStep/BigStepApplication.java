a189021c050a6a1eed0896861c4eb9f73e92f5bd
martintm03 <116195862+martintm03@users.noreply.github.com>
2024-06-13 12:34:58 +0200

    Mapeo de tablas y conexión con BD MySQL
    Se crean las principales clases del programa (Productos, Ordenes, Detalles de Orden y Usuarios), se hacen las relaciones entre las tablas y se configura el application.properties para la conexión con la base de datos en MySQL.

---
+++ MartinTaboada_BigStep_24/src/main/java/com/curso/BigStep/BigStepApplication.java
@@ -0,0 +1,12 @@
+package com.curso.BigStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BigStepApplication {	
+	public static void main(String[] args) {
+		SpringApplication.run(BigStepApplication.class, args);
+	}
+}
