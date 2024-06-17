bce0fd7b2f07859b8af53fe9b4ceb90bd50b8170
martintm03 <martin.t.m.327@gmail.com>
2024-06-17 19:19:08 +0200

    Arreglar scripts
    cambios en los scripts y application.properties para arreglar los scripts

---
+++ MartinTaboada_BigStep_24/src/main/resources/static/registro.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('registroForm').addEventListener('submit', function(event) {
+        let password = document.getElementById('password').value;
+        let regex = /^(?=.*[A-Z])(?=.*\d).{8,}$/; // Patrón para la contraseña
+
+        if (!regex.test(password)) {
+            alert('La contraseña debe contener al menos una letra mayúscula, un número y tener más de 8 caracteres.');
+            event.preventDefault(); // Evita que el formulario se envíe
+        } else {
+            // Aviso de éxito después de verificar la contraseña
+            alert('Cuenta creada correctamente.');
+        }
+    });
+});
---
+++ MartinTaboada_BigStep_24/target/classes/static/Contacto.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactoForm').addEventListener('submit', function(event) {
+        const mensaje = document.getElementById('mensaje').value;
+        if (mensaje.length < 30) {
+            alert('El mensaje debe tener al menos 30 caracteres.');
+            event.preventDefault();
+        } else {
+            alert('Formulario enviado correctamente.');
+        }
+    });
+});
